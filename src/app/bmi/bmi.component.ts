d4b6fa2aff1f969a24b1af3350be975e7eab90dd
yuko yuri <yuriko11d@gmail.com>
2020-08-19 22:24:52 +0900

    BMIの計算を実装する

--- src/app/bmi/bmi.component.ts
+++ src/app/bmi/bmi.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-bmi',
   templateUrl: './bmi.component.html',
-  styleUrls: ['./bmi.component.scss']
+  styleUrls: ['./bmi.component.scss'],
 })
 export class BmiComponent implements OnInit {
+  bmi = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  parseInt(str: string): number {
+    return parseInt(str);
   }
 
+  calcurate(height: number, weight: number) {
+    this.bmi = Math.round((weight / (height / 100) ** 2) * 10) / 10;
+  }
 }
